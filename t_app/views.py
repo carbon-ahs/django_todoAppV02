1488b6c1159caad51c943a804cf91044bcbec406
carbon-ahs <shehanuk.ahsan@gmail.com>
2021-07-30 23:23:41 +0600

    bootstrap added in static file

--- t_app/views.py
+++ t_app/views.py
@@ -5,8 +5,15 @@ from .models import
 from .forms import 
 '''
 def home(request):
-
     contex = {
         'key': 'value',
     }
     return render(request, 'home.html', contex)
+
+
+
+def about(request):
+    contex = {
+        'key': 'value',
+    }
+    return render(request, 'about.html', contex)
