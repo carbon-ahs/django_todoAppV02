2192f161e6439055117ef8dd86150ba402d5ab54
carbon-ahs <shehanuk.ahsan@gmail.com>
2021-08-03 00:07:53 +0600

    tut 3 finished

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+
+app_name = 'polls_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result/', views.result, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls_app/views.py
+++ polls_app/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, redirect
+from django.http.response import Http404
+from polls_app.models import Question
+from django.shortcuts import get_object_or_404, render, redirect
 
 '''
 from .models import 
 from .forms import 
 '''
 def home(request):
+
+    try:
+        all_questions = Question.objects.all()
+    except Exception as e:
+        raise Http404('No Qus Here!!')
+
     contex = {
         'key': 'value',
+        'all_questions': all_questions,
     }
     return render(request, 'home.html', contex)
 
@@ -17,3 +26,29 @@ def about(request):
         'key': 'value',
     }
     return render(request, 'about.html', contex)
+
+def detail(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    
+    
+    contex = {
+        'key': 'value',
+        'question': question,
+    }
+    return render(request, 'detail.html', contex)
+
+
+def result(request, question_id):
+    contex = {
+        'key': 'value',
+    }
+    return render(request, 'result.html', contex)
+
+
+def vote(request, question_id):
+    contex = {
+        'key': 'value',
+    }
+    return render(request, 'vote.html', contex)
