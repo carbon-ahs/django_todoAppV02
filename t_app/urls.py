4a220e654e0273e10992e3af99ae2e2978d8c552
carbon-ahs <shehanuk.ahsan@gmail.com>
2021-08-05 16:27:45 +0600

    t_app urls.py updated

--- t_app/urls.py
+++ t_app/urls.py
@@ -1,3 +1,12 @@
+'''
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+	path('', include('new_app.urls')),
+]
+'''
 from django.urls import path
 from . import views
 urlpatterns = [
